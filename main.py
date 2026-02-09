957a367f078c7bd9e5ec1920d643c098540ba3c4
toswa <sgricenko259@gmail.com>
2026-02-10 01:16:18 +0200

    Add a Tkinter GUI with color and font buttons

---
+++ main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+# 1. Створення вікна
+root = Tk()
+root.title("Цікаві кнопки")
+root.geometry("250x200")
+
+# --- Обробники подій ---
+def button_red():
+    root.config(bg="red")
+
+def button_blue():
+    root.config(bg="blue")
+
+def button_plus():
+    label_text.config(font="Arial 18")
+
+def button_minus():
+    label_text.config(font="Arial 10")
+
+def button_change():
+    label_text.config(text="Привіт від Python!")
+
+# 2. Кнопка "Червоний колір"
+b_red = Button(text="Червоний колір", command=button_red)
+b_red.pack(side=TOP, fill=X)
+
+# 3. Інші кнопки
+b_blue = Button(text="Синій колір", command=button_blue)
+b_blue.pack(side=TOP, fill=X)
+
+b_plus = Button(text="Збільшити шрифт", command=button_plus)
+b_plus.pack(side=TOP, fill=X)
+
+b_minus = Button(text="Зменшити шрифт", command=button_minus)
+b_minus.pack(side=TOP, fill=X)
+
+b_change = Button(text="Змінити напис", command=button_change)
+b_change.pack(side=TOP, fill=X)
+
+# 4. Напис унизу
+label_text = Label(
+    text="Hello, World!",
+    font="Arial 12",
+    fg="white",
+    bg="navy"
+)
+label_text.pack(side=BOTTOM, fill=X)
+
+# Завершення програми
+root.mainloop()
